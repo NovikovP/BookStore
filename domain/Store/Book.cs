80efcfd9bfedf66bcab9d2d6966120305c810722
Novikov Pavel <successdca@gmail.com>
2020-06-17 16:11:42 +0300

    писать сущность Book

---
+++ domain/Store/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Store
+{
+    public class Book
+    {
+        public string Title { get; }
+        public int Id { get; }
+
+        public Book(int id,string title)
+        {
+            Title = title;
+            Id = id;
+        }
+
+    }
+}
