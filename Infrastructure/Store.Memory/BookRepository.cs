4dfb6eae5568230e3c0446f28bb5bc18e6b82921
Novikov Pavel <successdca@gmail.com>
2020-06-17 18:13:43 +0300

    hot-fix

--- Infrastructure/Store.Memory/BookRepository.cs
+++ Infrastructure/Store.Memory/BookRepository.cs
@@ -10,6 +10,9 @@ namespace Store.Memory
             new Book(1, "Art of Programming"),
             new Book(2, "Refactoring"),
             new Book(3, "C Programming Language"),
+            new Book(4,"Popular programming"),
+            new Book (5,"Troelsen C#"),
+            new Book(6,"Shildt")
         };
 
         public Book[] GetAllByTitle(string titlePart)
--- presentation/Store.Web/Models/ErrorViewModel.cs
+++ presentation/Store.Web/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ using System;
 namespace Store.Web.Models
 {
     public class ErrorViewModel
-    {
+    {        
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
