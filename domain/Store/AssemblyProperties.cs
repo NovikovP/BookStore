bc80bedcee5c97e9f0c5cf5b721bdef00b2cdcc6
Novikov Pavel <successdca@gmail.com>
2020-06-22 23:52:51 +0300

    Add function

--- domain/Store/AssemblyProperties.cs
+++ domain/Store/AssemblyProperties.cs
@@ -1,3 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly:InternalsVisibleTo("Store.Test")]
+[assembly:InternalsVisibleTo("Store.Tests")]
--- domain/Store/IBookRepository.cs
+++ domain/Store/IBookRepository.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Store
 {
     public interface IBookRepository
     {
         Book[] GetAllByIsbn(string isbn);
-
+        
         Book[] GetAllByTitleOrAuthor(string titleOrAuthor);
+        
+        Book GetById(int id);
 
+        Book[] GetAllByIds(IEnumerable<int> bookIds);
     }
 }
